2152492270e71a5d453baa90cbf610e6b9d1b46d
Bang Nguyen <bangnv77@gmail.com>
2016-08-13 15:52:43 -0700

    create FoodComponent that will be in charge of displaying individual food itemds

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,14 +1,16 @@
 import { Component } from 'angular2/core';
 import { Food } from './food.model';
+import { FoodComponent } from './food.component';
 
 @Component({
   selector: 'food-list',
-  directives: [],
+  directives: [FoodComponent],
   inputs: ['foods'],
   pipes: [],
   template: `
     <div class="container">
       <h3 *ngFor="#food of foods">{{ food.description }} | {{ food.details }}</h3>
+      <food-display *ngFor="#currentFood of foods" [food]="currentFood" (click)="foodClicked(currentFood)"></food-display>
     </div>
   `
 })
@@ -18,6 +20,9 @@ export class FoodListComponent {
   public selectedFood: Food;
 
   constructor() {
-    
+    foodClicked(clickedFood: Food) {
+      this.selectedFood = clickedFood;
+      console.log(this.selectedFood);
+    }
   }
 }
---
+++ app/food.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+import { Food } from './food.model';
+
+@Component({
+  template: 'food-display',
+  inputs: ['foods'],
+  template: `
+    <div>
+      <p class="css-food">{{ food.description }} | {{ food.details }} | {{ food.calories}} | {{food.id}}</p>
+    </div>
+  `
+})
+
+export class FoodComponent {
+  public food: Food;
+}
