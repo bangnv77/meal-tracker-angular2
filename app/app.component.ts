295d969be5cf40603f703c909637c4162f75bd72
Bang Nguyen <bangnv77@gmail.com>
2016-08-13 15:24:37 -0700

    define Food Object in food.model and fill out app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from 'angular2/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'my-app',
+  directives: [],
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <h3 *ngFor="#food of foods">{{ food.description }} | {{ food.details }}</h3>
   `
 })
 
 export class AppComponent {
-
+  public foods: Food[];
+  constructor() {
+    this.foods = [
+      new Food('Pizza', 'delicious', 200, 0),
+      new Food('More Pizza', 'still delicious', 200, 1),
+      new Food('Even More Pizza', 'Ok', 250, 2),
+      new Food('Still More Pizza', 'A little full', 250, 3),
+      new Food('Why More Pizza?', 'barfy', -200, 4),
+      new Food('Pizza!', 'delicious', 200, 5),
+    ];
+  }
 }
---
+++ app/food.model.ts
@@ -0,0 +1,5 @@
+export class Food {
+  constructor(public description: string, public details: string, public calories: number, public id: number) {
+
+  }
+}
